781f763496f3b995e4bc27bfb9441a7e52badcc8
Robson Jean Penteado <robson@amplifique.me>
2023-02-06 16:05:51 -0300

    new in app support

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { StoreService } from '../_services/store.service';
-import { AmpCfService } from '@amplifique.me/ngx-amplifiqueme';
+import {
+  AmpCfService,
+  AmpSurveyService,
+} from '@amplifique.me/ngx-amplifiqueme';
 import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-preview',
@@ -13,17 +16,35 @@ export class PreviewComponent implements OnInit {
   constructor(
     public store: StoreService,
     public ampCfSurvey: AmpCfService,
+    public ampSurveyService: AmpSurveyService,
     public router: Router,
     public route: ActivatedRoute
   ) {}
 
-
   ngOnInit(): void {
-
     if (this.route.snapshot.params.survey) {
-      this.survey = this.route.snapshot.params.survey;
       this.showIframe = false;
-      setTimeout(()=>this.showNpsSurvey(), 1000)
+      this.survey = this.route.snapshot.params.survey;
+      let version = this.route.snapshot.queryParams.version || 1;
+      let identifier = this.route.snapshot.queryParams.identifier;
+      if (version == 2) {
+        this.ampSurveyService.load(identifier, true, false, true, this.survey).then(() => {
+          this.ampSurveyService.identify(
+            {
+              email: this.store.email,
+              name: this.store.name,
+              created_at: new Date().getTime(),
+            },
+            false
+          );
+          setTimeout(()=>{
+            this.ampSurveyService.run();
+
+          },2000)
+        });
+      }else{
+        setTimeout(() => this.showNpsSurvey(), 1000);
+      }
     } else {
       if (this.store.iframeUrl.length != 0) {
         this.showIframe = true;
@@ -40,7 +61,7 @@ export class PreviewComponent implements OnInit {
       name: this.store.name,
       survey: this.survey,
       created_at: new Date().getTime(),
-      force:true
+      force: true,
     });
 
     this.ampCfSurvey.run();
